fde599d78b15e969e7d8e7e95d6effdc5565a2ff
abduraxim <admabbasap@gmail.com>
2024-02-10 17:58:31 +0500

    pizza debugging step1

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App container">
       <Router>
         <Navbar />
         <Switch>
